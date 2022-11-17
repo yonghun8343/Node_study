4167b3c5435b3e293803c2dd876af939b78a1a6c
hun <yonghun83@naver.com>
2022-11-17 10:50:57 +0900

    .test 작성 동기화

--- 07 - 프로젝트/test/public/javascripts/index.js
+++ 07 - 프로젝트/test/public/javascripts/index.js
@@ -21,3 +21,43 @@ document.getElementById("forget_pwd").addEventListener("click", () => {
     findBox.style.transform = "scale(1)";
   }, 1200);
 });
+
+document.getElementById("find-arrow").addEventListener("click", () => {
+  const loginBox = document.getElementsByClassName("login-box")[0];
+  const findBox = document.getElementsByClassName("find_box")[0];
+
+  findBox.style.transition = "all 1s linear";
+  findBox.style.transform = "scale(0.5)";
+  findBox.style.opacity = 0;
+  setTimeout(() => {
+    findBox.style.display = "none";
+  }, 1000);
+
+  setTimeout(() => {
+    loginBox.style.display = "block";
+    loginBox.style.opacity = 0;
+    loginBox.style.transform = "scale(0.5)";
+  }, 1100);
+
+  setTimeout(() => {
+    loginBox.style.transition = "all 1s linear";
+    loginBox.style.opacity = 1;
+    loginBox.style.transform = "scale(1)";
+  }, 1200);
+});
+
+document.getElementById("find_submit").addEventListener("click", () => {
+  const submitBtn = document.getElementById("find_submit");
+
+  submitBtn.style.transition = "transform 1s linear";
+
+  if (document.getElementById("find_email").value) {
+    document.getElementById("find_valid").style.visibility = "visible";
+    submitBtn.style.transform = "translateY(-115px)";
+  }
+
+  if (document.getElementById("find_valid").value.length === 6) {
+    document.getElementById("find_pwd").style.visibility = "visible";
+    submitBtn.style.transform = "translateY(0px)";
+  }
+});
