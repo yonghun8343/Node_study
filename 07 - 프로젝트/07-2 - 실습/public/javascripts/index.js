d814daccce67a63b313df4f93c2ac4e86ffaf582
hun <yonghun83@naver.com>
2022-11-17 09:53:03 +0900

    add project js file

--- 07 - 프로젝트/07-2 - 실습/public/javascripts/index.js
+++ 07 - 프로젝트/07-2 - 실습/public/javascripts/index.js
@@ -1,3 +1,27 @@
+document.getElementById("forget_pwd").addEventListener("click", () => {
+  const loginBox = document.getElementsByClassName("login-box")[0];
+  const findBox = document.getElementsByClassName("find_box")[0];
+
+  loginBox.style.transition = "all 1s linear";
+  loginBox.style.transform = "scale(0.5)";
+  loginBox.style.opacity = 0;
+  setTimeout(() => {
+    loginBox.style.display = "none";
+  }, 1000);
+
+  setTimeout(() => {
+    findBox.style.display = "block";
+    findBox.style.opacity = 0;
+    findBox.style.transform = "scale(0.5)";
+  }, 1100);
+
+  setTimeout(() => {
+    findBox.style.transition = "all 1s linear";
+    findBox.style.opacity = 1;
+    findBox.style.transform = "scale(1)";
+  }, 1200);
+});
+
 document.getElementById("signup-submit").addEventListener("click", (target) => {
   const validBox = document.getElementById("valid");
   console.log(target);
@@ -5,7 +29,7 @@ document.getElementById("signup-submit").addEventListener("click", (target) => {
 
   const email = document.getElementById("email");
 
-  if (!!email.value) {
+  if (email.value) {
     validBox.style.display = "block";
     validBox.style.transition = "transform 1s linear";
     validBox.style.transform = "translateY(0px)";
@@ -29,7 +53,7 @@ document.getElementById("signup-submit").addEventListener("click", (target) => {
 });
 
 document.getElementById("find-submit").addEventListener("click", (target) => {
-  if (!!document.getElementById("find_email").value) {
+  if (document.getElementById("find_email").value) {
     target.target.style.transition = "transform 1s linear";
     target.target.style.transform = "translateY(0px)";
   }
@@ -52,8 +76,8 @@ document.getElementsByClassName("sign_box")[0].addEventListener("click", () => {
     singup.style.transform = "scale(0.5)";
   }, 1100);
   setTimeout(() => {
-    singup.style.opacity = 1;
     singup.style.transition = "all 1s linear";
+    singup.style.opacity = 1;
     singup.style.transform = "scale(1)";
   }, 1200);
 });
