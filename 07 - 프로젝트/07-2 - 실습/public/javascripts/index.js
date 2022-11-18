b9cb22780c04318b2d83755b419fc2c964e77a6d
hun <yonghun83@naver.com>
2022-11-18 09:06:52 +0900

    merge

--- 07 - 프로젝트/07-2 - 실습/public/javascripts/index.js
+++ 07 - 프로젝트/07-2 - 실습/public/javascripts/index.js
@@ -22,69 +22,49 @@ document.getElementById("forget_pwd").addEventListener("click", () => {
   }, 1200);
 });
 
-document.getElementById("signup-submit").addEventListener("click", (target) => {
-  const validBox = document.getElementById("valid");
-  console.log(target);
-  const submitBox = document.getElementById("signup-submit");
-
-  const email = document.getElementById("email");
-
-  if (email.value) {
-    validBox.style.display = "block";
-    validBox.style.transition = "transform 1s linear";
-    validBox.style.transform = "translateY(0px)";
-
-    submitBox.style.transition = "transform 1s linear";
-    submitBox.style.transform = "translateY(-235px)";
-  }
-
-  if (validBox.value.length === 6) {
-    document.getElementById("sign_pw").style.visibility = "visible";
-    submitBox.style.transform = "translateY(-175px)";
-    setTimeout(() => {
-      document.getElementById("sign_name").style.visibility = "visible";
-      submitBox.style.transform = "translateY(-115px)";
-    }, 900);
-    setTimeout(() => {
-      document.getElementById("sign_nick").style.visibility = "visible";
-      submitBox.style.transform = "translateY(-25px)";
-    }, 1800);
-  }
-});
-
-document.getElementById("find-submit").addEventListener("click", (target) => {
-  if (document.getElementById("find_email").value) {
-    target.target.style.transition = "transform 1s linear";
-    target.target.style.transform = "translateY(0px)";
-  }
-});
-
-document.getElementsByClassName("sign_box")[0].addEventListener("click", () => {
-  const loginBox = document.getElementsByClassName("login-box")[0];
-  const singup = document.getElementsByClassName("signup_box")[0];
-
-  loginBox.style.transition = "all 1s linear";
-  loginBox.style.transform = "scale(0.5)";
-  loginBox.style.opacity = 0;
-  setTimeout(() => {
-    loginBox.style.display = "none";
-  }, 1000);
-
-  setTimeout(() => {
-    singup.style.display = "block";
-    singup.style.opacity = 0;
-    singup.style.transform = "scale(0.5)";
-  }, 1100);
-  setTimeout(() => {
-    singup.style.transition = "all 1s linear";
-    singup.style.opacity = 1;
-    singup.style.transform = "scale(1)";
-  }, 1200);
-});
-
 document.getElementById("find-arrow").addEventListener("click", () => {
   const loginBox = document.getElementsByClassName("login-box")[0];
-  const find = document.getElementsByClassName("find_box")[0];
+  const findBox = document.getElementsByClassName("find_box")[0];
+
+  findBox.style.transition = "all 1s linear";
+  findBox.style.transform = "scale(0.5)";
+  findBox.style.opacity = 0;
+  setTimeout(() => {
+    findBox.style.display = "none";
+  }, 1000);
+
+  setTimeout(() => {
+    loginBox.style.display = "block";
+    loginBox.style.opacity = 0;
+    loginBox.style.transform = "scale(0.5)";
+  }, 1100);
+
+  setTimeout(() => {
+    loginBox.style.transition = "all 1s linear";
+    loginBox.style.opacity = 1;
+    loginBox.style.transform = "scale(1)";
+  }, 1200);
+});
+
+document.getElementById("find_submit").addEventListener("click", () => {
+  const submitBtn = document.getElementById("find_submit");
+
+  submitBtn.style.transition = "transform 1s linear";
+
+  if (document.getElementById("find_email").value) {
+    document.getElementById("find_valid").style.visibility = "visible";
+    submitBtn.style.transform = "translateY(-115px)";
+  }
+
+  if (document.getElementById("find_valid").value.length === 6) {
+    document.getElementById("find_pwd").style.visibility = "visible";
+    submitBtn.style.transform = "translateY(0px)";
+  }
+});
+
+document.getElementById("sign-up").addEventListener("click", () => {
+  const loginBox = document.getElementsByClassName("login-box")[0];
+  const signBox = document.getElementsByClassName("signup-box")[0];
 
   loginBox.style.transition = "all 1s linear";
   loginBox.style.transform = "scale(0.5)";
@@ -94,13 +74,72 @@ document.getElementById("find-arrow").addEventListener("click", () => {
   }, 1000);
 
   setTimeout(() => {
-    find.style.display = "block";
-    find.style.opacity = 0;
-    find.style.transform = "scale(0.5)";
+    signBox.style.display = "block";
+    signBox.style.opacity = 0;
+    signBox.style.transform = "scale(0.5)";
   }, 1100);
+
   setTimeout(() => {
-    find.style.opacity = 1;
-    find.style.transition = "all 1s linear";
-    find.style.transform = "scale(1)";
+    signBox.style.transition = "all 1s linear";
+    signBox.style.opacity = 1;
+    signBox.style.transform = "scale(1)";
   }, 1200);
+});
+
+document.getElementById("signup-arrow").addEventListener("click", () => {
+  const loginBox = document.getElementsByClassName("login-box")[0];
+  const signBox = document.getElementsByClassName("signup-box")[0];
+
+  signBox.style.transition = "all 1s linear";
+  signBox.style.transform = "scale(0.5)";
+  signBox.style.opacity = 0;
+  setTimeout(() => {
+    signBox.style.display = "none";
+  }, 1000);
+
+  setTimeout(() => {
+    loginBox.style.display = "block";
+    loginBox.style.opacity = 0;
+    loginBox.style.transform = "scale(0.5)";
+  }, 1100);
+
+  setTimeout(() => {
+    loginBox.style.transition = "all 1s linear";
+    loginBox.style.opacity = 1;
+    loginBox.style.transform = "scale(1)";
+  }, 1200);
+});
+
+let condition = 0;
+document.getElementById("signup-submit").addEventListener("click", () => {
+  const submitBtn = document.getElementById("signup-submit");
+
+  submitBtn.style.transition = "transform 1s linear";
+
+  if (document.getElementById("sign_email").value && condition === 0) {
+    condition += 1;
+    document.getElementById("sign_valid").style.visibility = "visible";
+    submitBtn.style.transform = "translateY(-240px)";
+  }
+
+  if (
+    document.getElementById("sign_valid").value.length === 6 &&
+    condition === 1
+  ) {
+    condition += 1;
+    document.getElementById("sign_pw").style.visibility = "visible";
+    submitBtn.style.transform = "translateY(-175px)";
+
+    console.log("2222222222222222");
+
+    setTimeout(() => {
+      document.getElementById("sign_name").style.visibility = "visible";
+      submitBtn.style.transform = "translateY(-115px)";
+    }, 900);
+
+    setTimeout(() => {
+      document.getElementById("sign_nick").style.visibility = "visible";
+      submitBtn.style.transform = "translateY(-25px)";
+    }, 1800);
+  }
 });
